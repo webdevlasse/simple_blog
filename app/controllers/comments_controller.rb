56240a896a790a5aca3d0999d36e7dcc148fdb5f
lasse christiansen <webdevlasse@gmail.com>
2012-12-05 11:16:44 -0800

    Update app/controllers/comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,6 @@
 class CommentsController < ApplicationController
   before_filter :authorize_user!
 
-  # POST /comments
-  # POST /comments.json
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.build(params[:comment])
@@ -10,23 +8,18 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment.article, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { redirect_to @comment.article }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
 
     respond_to do |format|
       format.html { redirect_to @comment.article }
-      format.json { head :no_content }
     end
   end
 
